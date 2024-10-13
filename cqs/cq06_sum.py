525e1d13a91de02ca7b106817c7f61b688bd62d0
lacycole <lacycole@unc.edu>
2024-10-13 18:08:04 -0400

    Finished ex04

--- cqs/cq06_sum.py
+++ cqs/cq06_sum.py
@@ -6,9 +6,9 @@ __author__ = "730735704"
 def w_sum(vals: list[float]) -> float:
     """Using a while loop"""
     index: int = 0
-    sum: float = 0.0
+    sum: float = 0.0  # keeps the value of the sum
     while index < len(vals):
-        sum += vals[index]
+        sum += vals[index]  # adds each value to the sum
         index += 1
     return sum
 
@@ -16,14 +16,16 @@ def w_sum(vals: list[float]) -> float:
 def f_sum(vals: list[float]) -> float:
     """Using for in"""
     sum: float = 0.0
-    for value in vals:
+    for value in vals:  # value is each extry in the list
         sum += value
     return sum
 
 
 def f_range_sum(vals: list[float]) -> float:
     """Using for in range"""
+    index: int = 0
     sum: float = 0.0
-    for value in range(0, len(vals)):
-        sum += value
+    for index in range(len(vals)):  # range has to relate to the index - not the values
+        sum += vals[index]  # adds to sum
+        index += 1
     return sum
